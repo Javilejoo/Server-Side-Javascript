1b24b6b30b46ae86756205c22ccad4f18dfe614d
Javilejoo <pra21486@uvg.edu.gt>
2024-03-05 20:02:21 -0600

    [IMPLEMENT] CORS

--- main.js
+++ main.js
@@ -1,34 +1,41 @@
 import express from 'express'
+import cors from 'cors'
 import { getAllPosts, createPost, getPostbyid, deletePostbyid, updatePostbyid} from './db.js'
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 const port = 3010
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+//obtener todos los posts
 app.get('/posts', async (req, res) => {
   const posts = await getAllPosts()
   res.json(posts)
 })
 
+//crear un post
 app.post('/posts', async (req, res) => {
   const posts = await createPost(req.body.title, req.body.content)
   res.json(posts)
 })
 
+//obtener un post por id
 app.post('/posts/:id', async (req, res) => {
   const posts = await getPostbyid(req.params.id)
   res.json(posts)
 })
 
+//borrar un post
 app.delete('/posts/:id', async (req, res) => {
   const post = await deletePostbyid(req.params.id)
   res.json(post)
 })
 
+//actualizar un post
 app.put('/posts/:id', async (req, res) => {
   const posts = await updatePostbyid(req.params.id, req.body.title, req.body.content)
   res.json(posts)
