5b2a8aa3a3673b4b2d67dfa3f90a3fc76daf01e1
Javilejo <pra21486@uvg.edu.gt>
2024-03-06 23:36:57 -0600

    [UPDATE] schema.sql

--- schema.sql
+++ schema.sql
@@ -9,22 +9,13 @@ FLUSH PRIVILEGES;
 CREATE TABLE characters (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
+    age INT,
     epithet VARCHAR(100),
     occupation VARCHAR(100),
-    bounty INT,
+    bounty BIGINT,
     devil_fruit VARCHAR(100),
-    image_url VARCHAR(255),
+    image_url TEXT,
+    image_base64 TEXT,
     description TEXT
 );
 
-
-{
-
-  "name": "Monkey D. Luffy",
-  "epithet": "Sombrero de Paja",
-  "occupation": "Pirata, Capitán",
-  "bounty": 3000000000,
-  "devil_fruit": "Gomu Gomu no Mi",
-  "image_url": "https://example.com/luffy.jpg",
-  "description": "Monkey D. Luffy es el protagonista principal de la serie de anime y manga One Piece. Es el capitán de la tripulación de los Piratas del Sombrero de Paja y se le conoce como 'Luffy del Sombrero de Paja'. Luffy es un joven valiente y aventurero que tiene como objetivo principal encontrar el legendario tesoro conocido como el 'One Piece' y convertirse en el Rey de los Piratas. Posee la Fruta del Diablo llamada Gomu Gomu no Mi, que le otorga la capacidad de estirar su cuerpo como si fuera de goma, lo que le permite realizar ataques elásticos y poderosos en combate."
-}
