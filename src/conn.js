d36f6a1dab946dab8944c4a6f8617783650a39f2
Javilejoo <pra21486@uvg.edu.gt>
2024-05-15 20:11:45 -0600

    [ADD] new database

--- src/conn.js
+++ src/conn.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise'
 
 const pool = mysql.createPool({
   host: 'sql5.freesqldatabase.com',
-  user: 'sql5704570',
-  database: 'sql5704570',
-  password: 'VFYH3JThxf',
+  user: 'sql5706790',
+  database: 'sql5706790',
+  password: '1y6vBZdVHl',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
